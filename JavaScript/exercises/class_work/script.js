0e9b2d62e7b0d1e5fd5ffbbf4ee9295c64131791
AJIBOLA (DON_GENIUS) AKELEBE <ajiboladolapogenius@gmail.com>
2026-04-28 12:42:05 +0100

    feat(JavaScript): add initial index.html and script.js for JavaScript class exercises

---
+++ JavaScript/exercises/class_work/script.js
@@ -0,0 +1,25 @@
+// Operators
+
+// Arithmetics
+
+let mod = 13 % 2
+let exp = 2 ** 16
+
+console.log(mod)
+console.log(exp)
+
+// Cpmparison
+console.log(`---Comparison Operators---`)
+
+console.log(null === undefined)
+console.log(null == undefined)
+
+// Logical
+console.log(`---Logical Operators---`)
+
+let username = ''
+console.log(username || 'Guest')
+// username = 'Christian'
+console.log(username || 'Guest')
+
+console.log(username ?? 'Guest')
