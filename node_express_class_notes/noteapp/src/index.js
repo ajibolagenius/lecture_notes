c263901edd0aaaa0572c5fe9fa522df64aa3a591
AJIBOLA (DON_GENIUS) AKELEBE <ajiboladolapogenius@gmail.com>
2026-08-20 15:44:27 +0100

    feat(routes): implement reminder routes for CRUD operations

--- node_express_class_notes/noteapp/src/index.js
+++ node_express_class_notes/noteapp/src/index.js
@@ -1,31 +1,11 @@
-import express from 'express'
+import express from 'express';
+import reminderRoutes from './routes/reminderRoutes.js';
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Defining the routes
-
-app.get('/reminders', (req, res) => {
-    res.send('Fetch all reminders.')
-})
-
-app.get('/reminders/:id', (req, res) => {
-    res.send('Fetch one reminder by id.')
-})
-
-app.post('/reminders', (req, res) => {
-    res.send('Create a new reminder.')
-})
-
-app.patch('/reminders/:id', (req, res) => {
-    res.send('Update some fields for existing reminder.')
-})
-
-app.delete('/reminders/:id', (req, res) => {
-    res.send('Delete a reminder.')
-})
-
+app.use('/api/v1/reminders', reminderRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Reminders API listening on port ${PORT}`)
-})
+  console.log(`Reminders API listening on port ${PORT}`);
+});
